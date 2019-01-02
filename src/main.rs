a5cf8087457576e2a9075dd2bb1bbaea53ccd5d5
Sudharshan D M <dmsudharshan@gmail.com>
2019-01-03 01:19:34 +0530

    Remove commented code and trailing whitespaces

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,6 @@ mod outcome {
         Loss,
         Draw,
     }
-
-    // impl Outcome {
-    //     pub fn display(&self) -> &str {
-    //         match self {
-    //             Outcome::Win => "Win",
-    //             Outcome::Loss => "Loss",
-    //             Outcome::Draw => "Draw",
-    //         }
-    //     }
-    // }    
 }
 
 use self::outcome::Outcome;
@@ -32,14 +22,6 @@ mod play {
     }
 
     impl Play {
-        // pub fn display(&self) -> &str {
-        //     return match self {
-        //         Play::Rock => "Rock",
-        //         Play::Paper => "Paper",
-        //         Play::Scissors => "Scissors",
-        //     };
-        // }
-
         pub fn from_char(c: &str) -> Option<Play> {
             return match c {
                 "r" => Some(Play::Rock),
@@ -53,7 +35,7 @@ mod play {
             match (self, other) {
                 (Play::Rock, Play::Scissors) => Outcome::Win,
                 (Play::Paper, Play::Rock) => Outcome::Win,
-                (Play::Scissors, Play::Paper) => Outcome::Win,        
+                (Play::Scissors, Play::Paper) => Outcome::Win,
                 (Play::Rock, Play::Paper) => Outcome::Loss,
                 (Play::Paper, Play::Scissors) => Outcome::Loss,
                 (Play::Scissors, Play::Rock) => Outcome::Loss,
@@ -87,34 +69,11 @@ fn main() {
     loop {
         println!("Turn {}:", n);
 
-        // let mut first_input = String::new();
-        // let first_play = loop {
-        //     println!("Enter first play:");
-        //     io::stdin().read_line(&mut first_input).expect("Error reading first play");
-        //     match Play::from_char(first_input.trim()) {
-        //         Some(play) => break play,
-        //         None => {
-        //             println!("Invalid play char: {}", first_input);
-        //             first_input.truncate(0)
-        //         },
-        //     };
-        // };
         let first_play = get_play("first");
         println!("{:?}", first_play);
         println!("");
 
         let second_play = get_play("second");
-        // let second_play = loop {
-        //     println!("Enter second play:");
-        //     io::stdin().read_line(&mut second_input).expect("Error reading second play");
-        //     match Play::from_char(second_input.trim()) {
-        //         Some(play) => break play,
-        //         None => {
-        //             println!("Invalid play char: {}", second_input);
-        //             second_input.truncate(0)
-        //         }
-        //     };
-        // };
         println!("{:?}", second_play);
         println!("");
 
@@ -129,11 +88,4 @@ fn main() {
             _ => break,
         }
     }
-
-    // match io::stdin().read_line(&mut input) {
-    //     Ok(_) => {
-    //         println!("{}", Play::from_char(input.trim()).display());
-    //     },
-    //     Err(error) => println!("error: {}", error),
-    // }
 }
