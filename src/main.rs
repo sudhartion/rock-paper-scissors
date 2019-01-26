0b2eeb8be852bb64afdfc2cbc8e7a057f7ded46f
Sudharshan D M <dmsudharshan@gmail.com>
2019-01-26 18:16:34 +0530

    Move modules to library

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,10 @@
+extern crate unnamed;
+
+use unnamed::{
+    outcome::Outcome,
+    play::Play,
+};
 use std::io;
-
-mod outcome {
-    #[derive(Debug)]
-    pub enum Outcome {
-        Win,
-        Loss,
-        Draw,
-    }
-}
-
-use self::outcome::Outcome;
-
-mod play {
-    use super::outcome::Outcome;
-
-    #[derive(Debug)]
-    pub enum Play {
-        Rock,
-        Paper,
-        Scissors,
-    }
-
-    impl Play {
-        pub fn from_char(c: &str) -> Option<Play> {
-            return match c {
-                "r" => Some(Play::Rock),
-                "p" => Some(Play::Paper),
-                "s" => Some(Play::Scissors),
-                _ => None,
-            };
-        }
-
-        pub fn fight(&self, other: &Play) -> Outcome {
-            match (self, other) {
-                (Play::Rock, Play::Scissors) => Outcome::Win,
-                (Play::Paper, Play::Rock) => Outcome::Win,
-                (Play::Scissors, Play::Paper) => Outcome::Win,
-                (Play::Rock, Play::Paper) => Outcome::Loss,
-                (Play::Paper, Play::Scissors) => Outcome::Loss,
-                (Play::Scissors, Play::Rock) => Outcome::Loss,
-                (Play::Rock, Play::Rock) => Outcome::Draw,
-                (Play::Paper, Play::Paper) => Outcome::Draw,
-                (Play::Scissors, Play::Scissors) => Outcome::Draw,
-            }
-        }
-    }
-}
-
-use self::play::Play;
 
 fn get_play(nth: &str) -> Play {
     let mut input = String::new();
